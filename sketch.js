a3356e2a8b87d8b41422ebae07a9786ab85bd399
Joel-make <77834323+Joel-make@users.noreply.github.com>
2021-04-29 17:10:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,26 @@
-var tom, tom_i;
-var jerry, jerry_i, jerry_i2;
-var bgImg;
 
-function preload()
-{
-  tom_i=loadImage("cat1.png");
-  jerry_i=loadImage("mouse1.png");
-  jerry_i2=loadAnimation("mouse3.png");
-  bgImg = loadImage("garden.png");
-  
+function preload() {
+    //load the images here
 }
 
-function setup ()
-{
-  createCanvas(1000,800);
-  tom=createSprite(800,600,10,10);
-  tom.addImage(tom_i);
-  tom.scale=0.2;
-  jerry=createSprite(400,600,10,10);
-  jerry.addImage(jerry_i);
-  jerry.scale=0.1;
- 
-}
-function draw()
-{
-  background(bgImg);
-  drawSprites();
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
 }
 
-function keyPressed()
-{
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
 
 
 }
